75f20561530d9e7051f6d9952596f7f6cfb94118
Allen <developer2019@icloud.com>
2020-05-12 02:35:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  let ranks = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let grandNode = document.querySelector('#grand-node');
+  let deepestNode = grandNode.children[0];
+  for (let i = 0; !deepestNode.children[i]; i) {
+    deepestNode = deepestNode.children[0];
+  }
+  return deepestNode;
+}
